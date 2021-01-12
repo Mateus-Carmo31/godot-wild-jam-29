7bc1937ddbe76968132910eb3a326fea6f184f13
Mateus-Carmo31 <mateus_oliveira31@outlook.com>
2021-01-11 23:58:50 -0300

    Implementado sistema básico de colocar peças. Ainda falta checar colisão e condições de vitória

--- game/Piece.gd
+++ game/Piece.gd
@@ -1,3 +1,19 @@
-extends Sprite
+extends CollisionObject2D
+class_name Piece
 
-export(Array, Vector2) var offsets
+export(Array, Vector2) var relative_spaces
+export(bool) var is_rotatable = true
+
+onready var original_position : Vector2 = position
+var is_placed = false
+
+signal selected
+signal deselected
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+		if event.is_pressed():
+			emit_signal("selected")
+#		else:
+#			emit_signal("deselected")
+
--- game/Table.gd
+++ game/Table.gd
@@ -1,40 +1,103 @@
 extends TileMap
 
-const TABLE_SIZE = Vector2(6,6)
+const TABLE_SIZE = Vector2(5,5)
 
-func _process(delta):
-	var mouse_hover_pos = world_to_map(get_local_mouse_position())
-	
-	var is_inside_x = mouse_hover_pos.x >= -TABLE_SIZE.x/2 && mouse_hover_pos.x < TABLE_SIZE.x/2
-	var is_inside_y = mouse_hover_pos.y >= -TABLE_SIZE.y/2 && mouse_hover_pos.y < TABLE_SIZE.y/2
-	
-	if(is_inside_x && is_inside_y):
-		update_table(mouse_hover_pos)
+var current_piece : Piece
 
-func update_table(pos):
+func _ready():
+	for child in get_child(0).get_children():
+		if child is Piece:
+			child.connect("selected", self, "select_piece", [child])
+#			child.connect("deselected", self, "deselect_piece")
+
+func _input(event):
+	# Deselects currently held piece. It's handled here to avoid some hiccups
+	if event is InputEventMouseButton:
+		if not event.is_pressed():
+			deselect_piece()
 	
-	# Get space occupied by piece (from center)
-	var offsets = get_piece_space()
-	
-	for y in range(-TABLE_SIZE.y/2, TABLE_SIZE.y/2):
-		for x in range(-TABLE_SIZE.x/2, TABLE_SIZE.x/2):
-			# Check center
-			if(Vector2(x,y) == pos):
-				set_cell(x, y, 2)
-				continue
-			else:
-				# Check offsets from center
-				var matched = false
-				for offset in offsets:
-					if(Vector2(x,y) == pos + offset):
-						set_cell(x, y, 2)
-						matched = true
-						break
-				
-				if(not matched):
-					set_cell(x, y, 0)
+	# Updates currently held cell position and table preview on mouse motion
+	if event is InputEventMouseMotion:
+		if(current_piece):
+			update_piece_pos(event.relative)
 			
-	print(pos, pos + offsets[0], pos + offsets[1])
+			if(is_in_board(world_to_map(current_piece.position.round()))):
+				update_table()
+
+func select_piece(piece):
+	current_piece = piece
+	current_piece.is_placed = false
+
+func update_piece_pos(pos):
+	current_piece.position += pos
+
+# Attempts to place a piece at a position.
+# TODO: check if space is occupied as well
+func try_place_piece():
+	
+	var piece_pos = world_to_map(current_piece.position.round())
+	
+	if not is_in_board(piece_pos):
+		return false
+	
+	for offset in get_piece_space():
+		if not is_in_board(piece_pos + offset):
+			return false
+	
+	return true
+
+func deselect_piece():
+	if(current_piece != null):
+		
+		var map_pos = world_to_map(current_piece.position.round())
+		
+		if(try_place_piece() == true):
+			current_piece.is_placed = true
+		else:
+			current_piece.position = current_piece.original_position
+			current_piece.is_placed = false
+		
+		current_piece = null
+		update_table()
+
+func update_table():
+	
+	var occupied_slots = []
+	
+	for piece in get_child(0).get_children():
+		if piece.is_placed:
+			var pos = world_to_map(piece.position.round())
+			occupied_slots.append(pos)
+			for offset in piece.relative_spaces:
+				occupied_slots.append(pos + offset)
+	
+	for y in range(-TABLE_SIZE.y/2, ceil(TABLE_SIZE.y/2)):
+		for x in range(-TABLE_SIZE.x/2, ceil(TABLE_SIZE.x/2)):
+			
+			# Cleans the space
+			set_cell(x, y, -1)
+			
+			# Draws an occupied slot if there is one
+			if occupied_slots.has(Vector2(x,y)):
+				set_cell(x, y, 1)
+			else:
+				# Draws the current piece placement preview (if there is one)
+				if current_piece:
+					var cur_piece_pos = world_to_map(current_piece.position.round())
+					
+					if(Vector2(x,y) == cur_piece_pos):
+						set_cell(x, y, 2)
+					
+					for offset in current_piece.relative_spaces:
+						if(Vector2(x,y) == cur_piece_pos + offset and is_in_board(cur_piece_pos + offset)):
+							set_cell(x, y, 2)
+						
 
 func get_piece_space():
-	return [Vector2(-1,0), Vector2(1,0)]
+	return current_piece.relative_spaces
+
+func is_in_board(pos):
+	var is_inside_x = pos.x >= -TABLE_SIZE.x/2 && pos.x < TABLE_SIZE.x/2
+	var is_inside_y = pos.y >= -TABLE_SIZE.y/2 && pos.y < TABLE_SIZE.y/2
+	
+	return is_inside_x and is_inside_y
